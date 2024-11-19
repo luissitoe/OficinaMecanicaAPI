84a082c15636053747c6d6a9d821447eed032423
Luís Sitoe <luisvasco.jr@outlook.com>
2024-11-19 06:08:25 +0200

    Update Models
    Added class Veiculo

---
+++ OficinaMecanicaAPI/Models/Veiculo.cs
@@ -0,0 +1,8 @@
+public class Veiculo {
+
+    public Guid Id { get; set; }
+    public required string Marca { get; set; }
+    public required string Modelo { get; set; }
+    public required string Matricula { get; set; }
+
+}
