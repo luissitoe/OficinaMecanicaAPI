1a6acbd95401cecaef73dba5c7922d130bbd5290
Luís Sitoe <luisvasco.jr@outlook.com>
2024-11-20 12:50:50 +0200

    Update Veiculo.cs

--- OficinaMecanicaAPI/Models/Veiculo.cs
+++ OficinaMecanicaAPI/Models/Veiculo.cs
@@ -1,9 +1,15 @@
-﻿namespace OficinaMecanicaAPI.Models
+
+﻿namespace OficinaMecanicaAPI.Models {
+
 public class Veiculo {
 
     public Guid Id { get; set; }
     public required string Marca { get; set; }
     public required string Modelo { get; set; }
+    public required string Cor { get; set; }
     public required string Matricula { get; set; }
+    public required Cliente cliente { get; set; }
+
+}
 
 }
